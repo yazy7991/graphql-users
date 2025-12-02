373bde909a5e802d630b53b5292fd047a3d420cf
yazy7991 <jj3369155@gmail.com>
2025-12-02 23:20:15 +0000

    Action: Setup the apollo server to listen for incoming request

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import {ApolloServer} from '@apollo/server';
+import {startStandalonServer} from '@apollo/server/standalone';
+
+// Create a new Apollo Server
+const server = new ApolloServer ({
+
+})
+
+// Set server to listen for incoming request from the Apollo Client
+const {url} = await startStandalonServer(server,{
+    listen: {port: 4000}
+})
+
+console.log(`Server running at: ${url}`);
+
+

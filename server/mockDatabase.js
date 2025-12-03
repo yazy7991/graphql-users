a156e7cc7d94815fefe8f3283dd3ec3d81109ce0
yazy7991 <jj3369155@gmail.com>
2025-12-03 22:13:34 +0000

    Create a fake database for testing graphql

---
+++ server/mockDatabase.js
@@ -0,0 +1,14 @@
+// Create 10 user information in a mock database
+
+export const users = [
+    {id: "1", name: "Jane Smith", age: 30, isMarried: true},
+    {id: "1", name: "Marco Polo", age: 10, isMarried: false},
+    {id: "1", name: "JGregory Alice", age: 35, isMarried: false},
+    {id: "1", name: "Joan Pharm", age: 20, isMarried: true},
+    {id: "1", name: "Robin williams", age: 25, isMarried: false},
+    {id: "1", name: "Peter Parker", age: 30, isMarried: true},
+    {id: "1", name: "Jane Grey", age: 28, isMarried: true},
+    {id: "1", name: "Captain America", age: 40, isMarried: false},
+    {id: "1", name: "Iron Man", age: 42, isMarried: true},
+    {id: "1", name: "Adam Sandler", age: 50, isMarried: false},
+]

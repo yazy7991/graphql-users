c1ecddba87ee5da9be83223c17078ece282c110f
yazy7991 <jj3369155@gmail.com>
2025-12-03 23:49:35 +0000

    Action: Setup the ApolloClient, InMemmoryCache and ApolloProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+
+const client = new ApolloClient({
+  // The URI contain the link to the Apollo Server
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+
+    </ApolloProvider>
+    
   </StrictMode>,
 )
